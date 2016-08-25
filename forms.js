6221abf12d5351c3070bf001c2c772bd83367fc0
nah <2trill2spill@gmail.com>
2016-08-25 12:51:52 -0500

    Translate from jquery to angular.

--- forms.js
+++ forms.js
@@ -1,75 +1,35 @@
-
 var employeeArray = [];
-var totalSalary = 0;
-var monthlyCost = 0;
 
-function RemoveHTML(id) {
-  $('#' + id).remove()
-}
+var app = angular.module("app", []);
 
-$(document).ready(function() {
-    var array = [];
-    $('#employeeinfo').on('submit', function(event) {
-      event.preventDefault();
+app.controller("formController", ["$scope", function($scope) {
+  console.log("Form controller works")
 
-      // initialize a new variable as an empty object
-      var values = {};
+  $scope.handleSubmit = function() {
+    var first = $scope.firstName;
+    var last = $scope.lastName;
+    var title = $scope.title;
+    var salary = $scope.salary;
+    var id = $scope.id;
+    var employee = new Employee(first, last, title, salary, id)
+    employeeArray.push(employee);
+    $scope.employeeArray = employeeArray;
+    clearInputs();
+  }
 
-      // convert the form inputs into an array
-      var fields = $('#employeeinfo').serializeArray();
-
-      // iterate over the array and transfer each index into a new property on an object with the value of what was entered.
-      fields.forEach(function(element, index, array) {
-        // review index notation vs. dot notation on objects
-        // here, dot notation wouldn't work
-        values[element.name] = element.value;
-      });
-      
-      // clear out inputs
-      $('#employeeinfo').find('input[type=text]').val('');
-
-      // append to DOM
-      appendDom(values);
-    });
-
-    function appendDom(values) {
-      var lastName = values["employeelastname"]
-      var firstName = values["employeefirstname"]
-      var jobTitle = values["jobTitle"]
-      var salary = values["salary"]
-      var id = values["id"]
-
-      var income = Number(salary)
-
-      totalSalary += income;
-      monthlyCost = Math.round(totalSalary / 12);
-
-      var e = Employee(firstName, lastName, jobTitle, salary, id)
-      employeeArray.push(e)
-
-      $('#container').append('<div></div>');
-      $('#container').children().last().attr('id', id);
-      var $el = $('#container').children().last();
-      $el.append('<p>' + firstName + ' ' + lastName + '</p>');
-      var $el = $('#container').children().last();
-      $el.append('<p>' + "Title:" + jobTitle + '</p>');
-      var $el = $('#container').children().last();
-      $el.append('<p>' + "ID:" + id + '</p>');
-      var $el = $('#container').children().last();
-      $el.append('<p>' + "Salary: $" + salary + '</p>');
-      var $el = $('#container').children().last();
-      $el.append('<p>' + "Total salary: $" + totalSalary + '</p>');
-      var $el = $('#container').children().last();
-      $el.append('<p>' + "Monthly salary cost: $" + monthlyCost + '</p>');
-      var $el = $('#container').children().last();
-      $el.append('<button onClick="RemoveHTML(' + id + ')">Delete</button>');
-    }
-});
+  function clearInputs() {
+    $scope.firstName = "";
+    $scope.lastName = "";
+    $scope.title = "";
+    $scope.salary = "";
+    $scope.id = "";
+  }
+}]);
 
 function Employee(first, last, title, salary, id) {
-  this.first = first
-  this.last = last
-  this.title = title
-  this.salary = salary
-  this.id
+  this.first = first;
+  this.last = last;
+  this.title = title;
+  this.salary = salary;
+  this.id = id;
 }
